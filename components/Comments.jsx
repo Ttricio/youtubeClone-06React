1163b8e200f341f9922cdf832a0de30d4079fb59
Patryk Kaczmarek <patrykmkaczmarek@gmail.com>
2022-11-08 14:10:06 +0100

    first commit

---
+++ components/Comments.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import Comment from "./Comment";
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  background-color: transparent;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+`;
+
+const Comments = () => {
+  return (
+    <Container>
+      <NewComment>
+        <Avatar src="https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+        <Input placeholder="Add a comment..." />
+      </NewComment>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+      <Comment/>
+    </Container>
+  );
+};
+
+export default Comments;
